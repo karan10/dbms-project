a176acdb4fc753e793ac525869c15e023186b2f8
karan <karandev43@gmail.com>
2014-03-22 01:55:35 +0530

    DBMS sample project

---
+++ index.php
@@ -0,0 +1,32 @@
+
+
+<html>
+<head>
+<title></title>
+</head>
+<body>
+<br />
+<h1 align="center">Welcome to Online Identification System</h1>
+<br /><br /><br /><br />
+<h2 style="font-family:Comic Sans Ms">Fill in Database<h2>
+<form method="POST" name="fill" action="entry.php">
+  <input type="submit" name="Submit"  value="fill me" />
+</form>
+
+<h2 style="font-family:Comic Sans Ms">Search in Database<h2>
+<form method="POST" name="view" action="view1.php">
+  <input type="submit" name="Submit"  value="view me" />
+</form>
+<br>
+
+<h4 align="left" style="font-family:Comic Sans Ms">By :
+<br>
+Akshay Jain (CS-05)<br>
+Karan Dev (CS-14)<br>
+Sajel Saxena (CS-28)<br>
+Manish Vashistha (IT-05)
+</h4>
+
+
+</body>
+</html>
